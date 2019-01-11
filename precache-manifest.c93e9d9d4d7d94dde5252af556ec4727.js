ebbb620c2fd258674b0458d9753f7235226a7b95
Krissy Dukes <krissydotnet@yahoo.com>
2019-01-11 07:19:14 -0500

    Updates

---
+++ precache-manifest.c93e9d9d4d7d94dde5252af556ec4727.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe5e503b1c6113920298",
+    "url": "/react-mad-libs/static/js/runtime~main.fe5e503b.js"
+  },
+  {
+    "revision": "db31be9daa8d74d0f7e6",
+    "url": "/react-mad-libs/static/js/main.db31be9d.chunk.js"
+  },
+  {
+    "revision": "bfd695c0315a8b678087",
+    "url": "/react-mad-libs/static/js/1.bfd695c0.chunk.js"
+  },
+  {
+    "revision": "db31be9daa8d74d0f7e6",
+    "url": "/react-mad-libs/static/css/main.bdeb6896.chunk.css"
+  },
+  {
+    "revision": "665ccdb0066956531ece84152b5cf35e",
+    "url": "/react-mad-libs/index.html"
+  }
+];
